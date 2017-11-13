0919e15f59436d6c254dbef80c75a286f233877e
Koen Kamman <kammankoen@gmail.com>
2017-11-13 13:50:40 +0100

    Everything up to 50

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test',
+      'https://cdn.pixabay.com/photo/2017/09/08/13/16/recipe-2728726_960_720.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test',
+      'https://cdn.pixabay.com/photo/2017/09/08/13/16/recipe-2728726_960_720.jpg')
+    ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
